168bcffa3c00c15e6c1524b27606c7a5755af4bc
Nilanshu V Rajmane <rajmanenilanshu@gmail.com>
2021-04-03 14:36:35 +0530

    removed extra file. checked useEffect with empty array

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 2. The first parameter takes a function which is called every render or based on second parameter
 3. The second parameter takes an array which contains the variables only for which the render needs to be called.
 4. The second parameter limits the call of useEffect based on the variables passed into the array.
+5. Passing no values to useEffect makes it call only once
--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ import './App.css';
 
 function App() {
 
-  const [values, handleChange] = useForm({ email: "", password: "" });
+  const [values, handleChange] = useForm({ email: "", password: "", firstName: "" });
 
   useEffect(() => {
     console.log("render");
-  })
+  }, []);
 
   return (
     <div className="App">
       Email: <input name="email" value={values.email} onChange={handleChange}></input>
+      First Name: <input name="firstName" value={values.firstName} onChange={handleChange}></input>
       Password: <input type="password" name="password" value={values.password} onChange={handleChange}></input>
       <div>
         Email: {values.email}, Password: {values.password}
