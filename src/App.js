3d0a210de796082580c21765a47d43b7c75f95bd
Nilanshu V Rajmane <rajmanenilanshu@gmail.com>
2021-04-03 17:45:15 +0530

    tested useState initialization

--- src/App.js
+++ src/App.js
@@ -7,10 +7,16 @@ import './App.css';
 
 function App() {
 
+  function getInitialValue() {
+    console.log('func called');
+    return JSON.parse(localStorage.getItem("count")||0);
+  }
+
   const [values, handleChange] = useForm({ email: "", password: "", firstName: "" });
   const [showHellow, toggleHello] = useState(true);
-  const [count, setCount] = useState(() => JSON.parse(localStorage.getItem("count")||0));
+  const [count, setCount] = useState(getInitialValue); //same as useState(() => JSON.parse(localStorage.getItem("count")||0))
   //using initiator function in useState because we don't want to call JSON.parse in every rerenders
+  // const [count, setCount] = useState(getInitialValue()); This calls the getInitialValue function in every rerender.
 
   useEffect(() => {
     localStorage.setItem("count", JSON.stringify(count));
