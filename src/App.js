ba73166e227c9984d12ae5058bc74ed9c15fa273
Nilanshu V Rajmane <rajmanenilanshu@gmail.com>
2021-04-09 15:33:17 +0530

    added a usecase for useMemo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import { useState } from 'react';
-
+import { useState, useMemo } from 'react';
+import useFetch from './useFetch';
 import './App.css';
+
+const computeLongestWord = (arr) => {
+  console.log('computeLongestWord called');
+
+  if (!arr) {
+    return '';
+  }
+
+  let longestWord = '';
+
+  arr.forEach(sentences =>
+    sentences.split(" ").forEach(word => {
+      if (word.length > longestWord.length) {
+        longestWord = word;
+      }
+    }))
+  return longestWord;
+}
 
 function App() {
 
   const [count, setCount] = useState(0);
 
+  const { data } = useFetch('https://raw.githubusercontent.com/ajzbc/kanye.rest/quotes/quotes.json');
+
+  const longestWord = useMemo(() => computeLongestWord(data), [data]); //computeLongestWord would be called multiple times without useMemo
+
   return (
     <div className="App">
       <div>Count: {count}</div>
-      <button onClick={()=>setCount(c => c+1)}>increment</button>
+      <button onClick={() => setCount(c => c + 1)}>increment</button>
+      <div>Longest Kanye Word: {longestWord}</div>
     </div>
   );
 }
--- src/useFetch.js
+++ src/useFetch.js
@@ -1,14 +1,13 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 
 const useFetch = (url) => {
 
-    const [state, setState] = useState({data: null, loading: true});
+    const [state, setState] = useState({ data: null, loading: true });
 
     useEffect(() => {
-        setState(currentState => ({data: currentState.data, loading: true}))
-        fetch(url)
-            .then(resp => resp.text())
-            .then(text => setState({data:text, loading: false}))
+        setState(currentState => ({ data: currentState.data, loading: true }));
+        fetch(url).then(resp => resp.json())
+            .then(text => setState({ data: text, loading: false }))
             .catch(console.log)
     }, [url]);
 
